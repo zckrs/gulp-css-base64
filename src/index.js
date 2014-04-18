82f7e2256008d43b2a7f5b24bf80fba8958ddc63
Mehdy Dara <mdara@eleven-labs.com>
2014-04-18 15:48:19 +0200

    Add .editorconfig

--- src/index.js
+++ src/index.js
@@ -22,67 +22,66 @@ var rImages = /url(?:\(['|"]?)(.*?)(?:['|"]?\))/img;
  * @return {string}      uri base64
  */
 function encodeImage(img, opts) {
-  var binImg = fs.readFileSync(img);
-  var mimeType = mime.lookup(img);
+    var binImg = fs.readFileSync(img);
+    var mimeType = mime.lookup(img);
 
-  var strImg = "data:" + mimeType + ";base64," + binImg.toString("base64");
+    var strImg = "data:" + mimeType + ";base64," + binImg.toString("base64");
 
-  return strImg;
+    return strImg;
 }
 
 function gulpCssBase64() {
 
-  // Creating a stream through which each file will pass
-  var stream = through.obj(function(file, enc, callback) {
+    // Creating a stream through which each file will pass
+    var stream = through.obj(function (file, enc, callback) {
 
-    if (file.isNull()) {
-       // Do nothing if no contents
-    }
-    if (file.isBuffer()) {
-      var src = file.contents.toString();
-      var result = [];
-      var resultEncoded = [];
-      var location = '';
-
-      async.whilst(
-        function () {
-          result = rImages.exec(src);
-
-          return result !== null;
-        },
-        function (callback) {
-          console.log(gutil.colors.green(result[1]));
-
-          location = path.join(path.dirname(file.path), result[1]);
-
-          if (!fs.existsSync(location)) {
-            gutil.log("Ressource not found : " + gutil.colors.white.bgRed(location));
-          }
-          else {
-            src = src.replace(result[1], encodeImage(location, null));
-          }
-
-          callback();
-        },
-        function () {
-          console.log(gutil.colors.cyan.bold('DONE !'));
+        if (file.isNull()) {
+            // Do nothing if no contents
         }
-      );
+        if (file.isBuffer()) {
+            var src = file.contents.toString();
+            var result = [];
+            var resultEncoded = [];
+            var location = '';
 
-      file.contents = new Buffer(src);
-    }
+            async.whilst(
+                function () {
+                    result = rImages.exec(src);
 
-    if (file.isStream()) {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'Stream not supported!'));
-    }
+                    return result !== null;
+                },
+                function (callback) {
+                    console.log(gutil.colors.green(result[1]));
 
-    this.push(file);
+                    location = path.join(path.dirname(file.path), result[1]);
 
-    return callback();
-  });
+                    if (!fs.existsSync(location)) {
+                        gutil.log("Ressource not found : " + gutil.colors.white.bgRed(location));
+                    } else {
+                        src = src.replace(result[1], encodeImage(location, null));
+                    }
 
-  // returning the file stream
-  return stream;
+                    callback();
+                },
+                function () {
+                    console.log(gutil.colors.cyan.bold('DONE !'));
+                }
+            );
+
+            file.contents = new Buffer(src);
+        }
+
+        if (file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Stream not supported!'));
+        }
+
+        this.push(file);
+
+        return callback();
+    });
+
+    // returning the file stream
+    return stream;
 };
 
 // Exporting the plugin main function
