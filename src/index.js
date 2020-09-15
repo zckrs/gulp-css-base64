83eb0382aac979b7ef0be83d5924f8ea197c2053
Mehdy Dara <mehdy.dara.fr@gmail.com>
2020-09-15 12:05:51 +0200

    Update ESLint and fix

--- src/index.js
+++ src/index.js
@@ -22,22 +22,21 @@ var rImages = /url(?:\(['|"]?)(.*?)(?:['|"]?\))(?!.*\/\*base64:skip\*\/)/ig;
 function gulpCssBase64(opts) {
   opts = JSON.parse(JSON.stringify(opts || {}));
   opts.maxWeightResource = opts.maxWeightResource || 32768;
-  if (util.isArray(opts.extensionsAllowed)) {
-    opts.extensionsAllowed = opts.extensionsAllowed;
-  } else {
+  if (!util.isArray(opts.extensionsAllowed)) {
     opts.extensionsAllowed = [];
   }
+
   opts.extensionsAllowed = opts.extensionsAllowed || [];
   opts.baseDir = opts.baseDir || '';
   opts.verbose = process.argv.indexOf('--verbose') !== -1;
 
-    // Creating a stream through which each file will pass
+  // Creating a stream through which each file will pass
   var stream = through.obj(function (file, enc, callbackStream) {
     var currentStream = this;
     var cache = [];
 
     if (file.isNull()) {
-            // Do nothing if no contents
+      // Do nothing if no contents
       currentStream.push(file);
 
       return callbackStream();
@@ -48,49 +47,50 @@ function gulpCssBase64(opts) {
       var result = [];
 
       async.whilst(
-                function () {
-                  result = rImages.exec(src);
+        function () {
+          result = rImages.exec(src);
 
-                  return result !== null;
-                },
-                function (callback) {
-                  if (cache[result[1]]) {
-                    src = src.replace(result[1], cache[result[1]]);
-                    callback();
-                    return;
-                  }
+          return result !== null;
+        },
+        function (callback) {
+          if (cache[result[1]]) {
+            src = src.replace(result[1], cache[result[1]]);
+            callback();
+            return;
+          }
 
-                  var pureUrl = result[1].split('?')[0].split('#')[0];
-                  if (opts.extensionsAllowed.length !== 0 && opts.extensionsAllowed.indexOf(path.extname(pureUrl)) === -1) {
-                    log('Ignores ' + chalk.yellow(result[1]) + ', extension not allowed ' + chalk.yellow(path.extname(result[1])), opts.verbose);
-                    callback();
-                    return;
-                  }
+          var pureUrl = result[1].split('?')[0].split('#')[0];
+          if (opts.extensionsAllowed.length !== 0 && opts.extensionsAllowed.indexOf(path.extname(pureUrl)) === -1) {
+            log('Ignores ' + chalk.yellow(result[1]) + ', extension not allowed ' + chalk.yellow(path.extname(result[1])), opts.verbose);
+            callback();
+            return;
+          }
 
-                  encodeResource(result[1], file, opts, function (fileRes) {
-                    if (undefined !== fileRes) {
-                      if (fileRes.contents.length > opts.maxWeightResource) {
-                        log('Ignores ' + chalk.yellow(result[1]) + ', file is too big ' + chalk.yellow(fileRes.contents.length + ' bytes'), opts.verbose);
-                        callback();
-                        return;
-                      }
+          encodeResource(result[1], file, opts, function (fileRes) {
+            if (undefined !== fileRes) {
+              if (fileRes.contents.length > opts.maxWeightResource) {
+                log('Ignores ' + chalk.yellow(result[1]) + ', file is too big ' + chalk.yellow(fileRes.contents.length + ' bytes'), opts.verbose);
+                callback();
+                return;
+              }
 
-                      var strRes = 'data:' + mime.lookup(fileRes.path) + ';base64,' + fileRes.contents.toString('base64');
-                      src = src.replace(result[1], strRes);
+              var strRes = 'data:' + mime.lookup(fileRes.path) + ';base64,' + fileRes.contents.toString('base64');
+              src = src.replace(result[1], strRes);
 
-                            // Store in cache
-                      cache[result[1]] = strRes;
-                    }
-                    callback();
-                  });
-                },
-                function () {
-                  file.contents = new Buffer(src);
-                  currentStream.push(file);
+              // Store in cache
+              cache[result[1]] = strRes;
+            }
 
-                  return callbackStream();
-                }
-            );
+            callback();
+          });
+        },
+        function () {
+          file.contents = Buffer.from(src);
+          currentStream.push(file);
+
+          return callbackStream();
+        }
+      );
     }
 
     if (file.isStream()) {
@@ -98,7 +98,7 @@ function gulpCssBase64(opts) {
     }
   });
 
-    // returning the file stream
+  // Returning the file stream
   return stream;
 }
 
@@ -119,7 +119,7 @@ function encodeResource(img, file, opts, doneCallback) {
 
   if (/^(http|https|\/\/)/.test(img)) {
     log('Fetch ' + chalk.yellow(img), opts.verbose);
-        // different case for uri start '//'
+    // Different case for uri start '//'
     if (img[0] + img[1] === '//') {
       img = 'http:' + img;
     }
@@ -130,10 +130,10 @@ function encodeResource(img, file, opts, doneCallback) {
         doneCallback();
         return;
       }
+
       fileRes.path = img;
       fileRes.contents = resultBuffer;
       doneCallback(fileRes);
-      return;
     });
   } else {
     var location = '';
@@ -154,7 +154,6 @@ function encodeResource(img, file, opts, doneCallback) {
     fileRes.contents = binRes;
 
     doneCallback(fileRes);
-    return;
   }
 }
 
@@ -165,8 +164,9 @@ function fetchRemoteRessource(url, callback) {
 
   imageStream.writable = true;
   imageStream.write = function (data) {
-    buffList.push(new Buffer(data));
+    buffList.push(Buffer.from(data));
   };
+
   imageStream.end = function () {
     resultBuffer = buffList.toBuffer();
   };
@@ -177,7 +177,7 @@ function fetchRemoteRessource(url, callback) {
       return;
     }
 
-        // Bail if we get anything other than 200
+    // Bail if we get anything other than 200
     if (response.statusCode !== 200) {
       callback(null);
       return;
